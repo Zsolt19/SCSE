1b29b2f0c7bf2350123e4884251c35c569986aa0
Zsolt19 <zsolt@levellada.net>
2025-01-14 11:44:03 +0000

    UPD class practice

--- Week3/Day2/practicew3d2.py
+++ Week3/Day2/practicew3d2.py
@@ -1,5 +1,14 @@
 class Student:
-    def __init__(self, name, age, grade)
+    def __init__(self, name, age, grade, height):
         self.name=name
         self.age=age
         self.grade=grade
+        self.height=height
+    def __str__(self):
+        return f"Student name: {self.name}, Age:{self.age}, Grade: {self.grade}, Height: {self.height}"
+
+new_student=Student("Calum",20,"A",165)
+old_student=Student("Dan",18,"A",164)
+print(new_student.name)
+print(old_student.grade)
+print(new_student)
